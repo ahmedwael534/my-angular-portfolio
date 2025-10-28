556fccc1087c0af0c5916d2a600592dac936af8d
Ahmed Wael <awael534@gmail.com>
2025-10-28 21:35:55 +0300

    Fix  background image using property binding

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -9,10 +9,13 @@ import { CommonModule } from '@angular/common';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+
+  backgroundImageUrl: string = 'assets/images/profile.png';
+
   profile = {
     name: "Ahmed Wael Mohamed Elawady",
     title: "Computers and Systems Engineer | Full-Stack .NET Developer Trainee",
-    summary: "I’m a highly motivated Computers and Systems Engineering graduate with Excellent with Honors from the Faculty of Engineering. I’m passionate about using technology to create smart and practical solutions that make an impact. In 2023, I trained at Route IT Training Center, where I studied Flutter and gained hands-on experience in mobile app development. Currently, I’m expanding my skillset as a Full-Stack .NET Developer trainee at ITI – Alexandria branch, building a stronger foundation in software engineering and web technologies. During my academic journey, I worked on an AI/IoT-based Water Level Prediction System, a project that combined innovation with real-world problem-solving. I enjoy working in collaborative environments and bring strong problem-solving, communication, and time management skills to every team I join. I’m always eager to learn, grow, and take on new challenges in today’s dynamic tech world.",
+    summary: `I’m a highly motivated Computers and Systems Engineering graduate (Excellent w/ Honors) passionate about leveraging technology to create impactful solutions. Following my graduation in July 2024, I completed a Flutter Development Diploma at Route IT Training Center (Nov 2023). Currently enhancing my skillset as a Full-Stack .NET Developer trainee at ITI – Alexandria branch, focusing on robust software engineering and web technologies. My academic journey included leading the development of an AI/IoT-based Water Level Prediction System, demonstrating innovation and practical problem-solving. I thrive in collaborative environments, bringing strong analytical, communication, and time management skills. Always eager to learn, grow, and embrace new challenges in the dynamic tech world.`,
     track: "Information Technology Institute (ITI) - Full-Stack .NET Development Track"
   };
 
@@ -32,4 +35,5 @@ export class AboutComponent {
     ]
   };
 }
+
 
