95df1d923452cc8847343049338fb9b43d9ecc32
Ahmed Wael <awael534@gmail.com>
2025-10-28 20:10:00 +0300

    Configure HashLocationStrategy for GitHub Pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,9 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 
 import { routes } from './app.routes';
 
@@ -7,8 +11,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
 
-    importProvidersFrom(ReactiveFormsModule)
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(ReactiveFormsModule),
+    provideAnimationsAsync()
   ]
 };
